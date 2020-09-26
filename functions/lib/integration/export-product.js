30a932d2666b1f843aa77f5a8598a24bbc99756c
Leonardo Matos <leomp120894@gmail.com>
2020-09-26 16:08:51 -0300

    fix(export-product): fix handling (bad) ideris api response on products search
    it returns 400 when product is not found (wdf ?)

--- functions/lib/integration/export-product.js
+++ functions/lib/integration/export-product.js
@@ -16,21 +16,28 @@ module.exports = ({ appSdk, storeId }, iderisLoginToken, queueEntry, iderisProdu
       const ideris = new Ideris(iderisLoginToken)
       const job = ideris.preparing
         .then(() => {
-          return ideris.axios.get(`/Produto/?sku=${product.sku}`).then(({ data }) => {
-            if (Array.isArray(data.result)) {
-              const iderisProduct = data.result.find(({ sku }) => sku === product.sku)
-              let method
-              if (iderisProduct) {
-                iderisProductPayload = {
-                  id: iderisProduct.id
-                }
-                method = 'put'
-              } else {
-                method = 'post'
+          return ideris.axios.get(`/Produto/?sku=${product.sku}`)
+            .catch(err => {
+              if (err.response && err.response.status === 400) {
+                return { result: [] }
               }
-              return ideris.axios[method]('/Produto', parseProduct(product, iderisProductPayload))
-            }
-          })
+              throw err
+            })
+            .then(({ data }) => {
+              if (Array.isArray(data.result)) {
+                const iderisProduct = data.result.find(({ sku }) => sku === product.sku)
+                let method
+                if (iderisProduct) {
+                  iderisProductPayload = {
+                    id: iderisProduct.id
+                  }
+                  method = 'put'
+                } else {
+                  method = 'post'
+                }
+                return ideris.axios[method]('/Produto', parseProduct(product, iderisProductPayload))
+              }
+            })
         })
         .catch(console.error)
       handleJob({ appSdk, storeId }, queueEntry, job)
