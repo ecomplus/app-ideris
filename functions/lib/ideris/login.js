e0b948f6c182a85734169d3c21b7a19eedcfd825
Leonardo Matos <leomp120894@gmail.com>
2020-09-26 16:55:54 -0300

    fix(ideris-login): delay and retry on first error

--- functions/lib/ideris/login.js
+++ functions/lib/ideris/login.js
@@ -1,8 +1,18 @@
 const axios = require('./create-axios')()
 
 // https://documenter.getpostman.com/view/4554319/S1a63SJM?version=latest#151ba154-9c6e-4eaf-959c-3f14e5d5d614
-module.exports = loginToken => axios
-  .post('/Login', {
-    login_token: loginToken
-  })
-  .then(({ data }) => data)
+module.exports = loginToken => new Promise((resolve, reject) => {
+  const request = isRetry => {
+    axios.post('/Login', {
+      login_token: loginToken
+    })
+      .then(({ data }) => resolve(data))
+      .catch(err => {
+        if (!isRetry && err.response && err.response.status >= 429) {
+          setTimeout(() => request(true), 7000)
+        }
+        reject(err)
+      })
+  }
+  request()
+})
